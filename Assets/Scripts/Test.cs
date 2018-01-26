0d3cd6231681a7fe81745606fec3a47cfe04d3a1
RHONE\Diego <diegoossa@gmail.com>
2018-01-26 12:33:47 -0500

    Dev Branch

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class Test : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        print("Sizas por eso");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
