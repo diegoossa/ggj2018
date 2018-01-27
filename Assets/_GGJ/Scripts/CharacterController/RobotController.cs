088ff677664df6c1de2ee38cac446c1b105f1aff
ROG\Diego Ossa <diegoossa@gmail.com>
2018-01-27 00:39:12 -0500

    RobotCharacter controller and Gravity

---
+++ Assets/_GGJ/Scripts/CharacterController/RobotController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GravityBody))]
+public class RobotController : MonoBehaviour
+{
+    public float walkSpeed = 6;
+    public float jumpForce = 220;
+    public LayerMask groundedMask;
+
+    // System vars
+    private bool grounded;
+    private Vector3 moveAmount;
+    private Vector3 smoothMoveVelocity;
+    private float verticalLookRotation;
+    private Transform cameraTransform;
+    private Rigidbody rigidbody;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Calculate movement:
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        // Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                rigidbody.AddForce(transform.up * jumpForce);
+            }
+        }
+
+        // Grounded check
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1 + .1f, groundedMask))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement to rigidbody
+        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+        rigidbody.MovePosition(rigidbody.position + localMove);
+    }
+}
